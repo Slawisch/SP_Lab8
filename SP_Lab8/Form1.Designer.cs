72f9b4f8d875111eca806afb83261fe962c459a0
Slawisch <dmitry.js01@gmail.com>
2020-12-21 11:09:53 +0200

    Initial commit

---
+++ SP_Lab8/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace SP_Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button1.Location = new System.Drawing.Point(5, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Apply new color";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button2.Location = new System.Drawing.Point(5, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 24);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Pick color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button3.Location = new System.Drawing.Point(5, 72);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(140, 24);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Backup changes";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label1.Location = new System.Drawing.Point(151, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Picked color:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(251, 43);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(21, 22);
+            this.panel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label2.Location = new System.Drawing.Point(151, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Current color:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(251, 14);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(21, 22);
+            this.panel2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, ((System.Drawing.FontStyle) ((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label3.Location = new System.Drawing.Point(151, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Default color:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(251, 73);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(21, 22);
+            this.panel3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 104);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Panel panel3;
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+
+        private System.Windows.Forms.Panel panel1;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+
+        #endregion
+    }
+}
---
+++ SP_Lab8/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace SP_Lab8
+{
+    public partial class Form1 : Form
+    {
+        private RegistryKey hkColors;
+        private Color currentColor = DefaultBackColor;
+        private Color pickedColor = DefaultBackColor;
+        private Color defaultColor = DefaultBackColor;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            RegistryKey hkCurrentUser = Registry.CurrentUser;
+            RegistryKey hkControlPanel = hkCurrentUser.OpenSubKey("Control Panel");
+            hkColors = hkControlPanel.OpenSubKey("Colors",true);
+            object bgColor = hkColors.GetValue("Background");
+            
+            string[] currentColorStrings = bgColor.ToString().Split(' ');
+            
+            currentColor = Color.FromArgb(Int32.Parse(currentColorStrings[0]), Int32.Parse(currentColorStrings[1]),
+                Int32.Parse(currentColorStrings[2]));
+            pickedColor = currentColor;
+            defaultColor = currentColor;
+
+            panel2.BackColor = currentColor;
+            panel1.BackColor = pickedColor;
+            panel3.BackColor = defaultColor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hkColors.SetValue("Background", $"{pickedColor.R} {pickedColor.G} {pickedColor.B}");
+            currentColor = pickedColor;
+            panel2.BackColor = currentColor;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = pickedColor;
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            pickedColor = colorDialog1.Color;
+            panel1.BackColor = pickedColor;
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            hkColors.SetValue("Background", $"{defaultColor.R} {defaultColor.G} {defaultColor.B}");
+            currentColor = defaultColor;
+            panel2.BackColor = currentColor;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Apply changes?",
+                "Exit",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information) == DialogResult.No) {
+                hkColors.SetValue("Background", $"{defaultColor.R} {defaultColor.G} {defaultColor.B}");
+            }
+        }
+    }
+}
